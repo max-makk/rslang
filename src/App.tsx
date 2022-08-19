179e9ca2ec63e5f96437f9956da5387c671c9da6
Kate Hubar <70864294+KateHubar@users.noreply.github.com>
2022-08-19 19:23:45 +0300

    feat: add footer files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Home } from './pages/Home/Home';
+import { Footer } from './components/footer/Footer';
 import './App.css';
 
 function App() {
   return <>
     <Home />
+    <Footer />
   </>;
 }
 
