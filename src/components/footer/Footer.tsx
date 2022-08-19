a0e48f7afe32eae60fe23a8f42a2a653ee1f396a
Kate Hubar <70864294+KateHubar@users.noreply.github.com>
2022-08-19 22:08:17 +0300

    feat: add footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,5 +1,21 @@
-import style from './Footer.module.css'
+import style from './Footer.module.css';
 
 export const Footer = () => {
-  return <div className={style.footer} >Footer</div>
-}
+  return (
+    <div className={style.footer}>
+      <p>&copy; 2022</p>
+      <ul className={style.footer_list}>
+        <li>
+          <a href="https://github.com/max-makk" target="_blank"> @max-makk </a>
+        </li>
+        <li>
+          <a href="https://github.com/KateHubar" target="_blank"> @KateHubar </a>
+        </li>
+        <li>
+          <a href="https://github.com/tankost" target="_blank"> @tankost </a>
+        </li>
+      </ul>
+      <a className={style.footer_rss} target="_blank" href="https://rs.school/js/"></a>
+    </div>
+  );
+};
