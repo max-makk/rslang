3f76f80f88d64607c05fb537ed012bb0f129e42d
Kate Hubar <70864294+KateHubar@users.noreply.github.com>
2022-08-22 02:50:45 +0300

    feat: add team description

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,26 @@
-import style from './Home.module.css'
+import style from './Home.module.css';
 
 export const Home = () => {
-  return <div className={style.div}>hi</div>
-}
+  return (
+    <div className={style.team_wrapper}>
+      <div className={style.team_string}>
+        <a href="https://github.com/max-makk" target="_blank" rel="noreferrer">
+          <div className={style.team_ava + ' ' + style.team_ava1}></div>
+        </a>
+        <article className={style.team_text}>Team Lead, Frontend developer. Координация работы комды. Разработка архитектуры приложения. Настройка сервера и работы с ним.</article>
+      </div>
+      <div className={style.team_string}>
+        <a href="https://github.com/KateHubar" target="_blank" rel="noreferrer">
+          <div className={style.team_ava + ' ' + style.team_ava2}></div>
+        </a>
+        <article className={style.team_text}>Frontend developer. Разработка главной страницы и формы авторизации.</article>
+      </div>
+      <div className={style.team_string}>
+        <a href="https://github.com/tankost" target="_blank" rel="noreferrer">
+          <div className={style.team_ava + ' ' + style.team_ava3}></div>
+        </a>
+        <article className={style.team_text}>Frontend developer. Разработка хедера и меню. Разработка словаря. </article>
+      </div>
+    </div>
+  );
+};
