016f04394f708f9e2b10332afd4d7d98d7c31212
Kate Hubar <70864294+KateHubar@users.noreply.github.com>
2022-08-22 21:31:56 +0300

    fix: footer adaptive

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,16 +6,16 @@ export const Footer = () => {
       <p>&copy; 2022</p>
       <ul className={style.footer_list}>
         <li>
-          <a href="https://github.com/max-makk" target="_blank"> @max-makk </a>
+          <a href="https://github.com/max-makk" target="_blank" rel="noreferrer"> @max-makk </a>
         </li>
         <li>
-          <a href="https://github.com/KateHubar" target="_blank"> @KateHubar </a>
+          <a href="https://github.com/KateHubar" target="_blank" rel="noreferrer"> @KateHubar </a>
         </li>
         <li>
-          <a href="https://github.com/tankost" target="_blank"> @tankost </a>
+          <a href="https://github.com/tankost" target="_blank" rel="noreferrer"> @tankost </a>
         </li>
       </ul>
-      <a className={style.footer_rss} target="_blank" href="https://rs.school/js/"></a>
+      <a className={style.footer_rss} target="_blank" href="https://rs.school/js/" rel="noreferrer"></a>
     </div>
   );
 };
