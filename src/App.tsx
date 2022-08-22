12827525c970f64d4a58000c834f49a972fcf3d2
max-makk <90equals50@gmail.com>
2022-08-22 13:52:46 +0300

    feat: replace App function to arrow

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Footer } from './components/footer/Footer';
 import './App.css';
 import {Header} from './components/Header';
 
-function App() {
+const App = () => {
   return <>
     <Header/>
     <Home/>
