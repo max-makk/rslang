ebbdbf73e74fe28dc8222806e7ef9a3b66bf3ed4
Tatiana Kostukevich <tankostuk@gmail.com>
2022-08-22 13:18:06 +0300

    refactor: add react-router-dom to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,26 @@
 import style from './Header.module.css';
+import { Link, useMatch, useResolvedPath } from 'react-router-dom';
 
 export const Header = () => {
   return <header className={style.header}>
-    <a href={'/'} className={style.headerLink + ' ' + style.headerLogo}>RSlang</a>
+    <Link to={'/'} className={style.headerLink + ' ' + style.headerLogo}>RSlang</Link>
     <nav className={style.headerNav}>
       <ul className={style.headerList}>
-        <CustomLink href={'/'} className={style.headerLink}>Главная</CustomLink>
-        <CustomLink href={'/textbook'} className={style.headerLink}>Учебник</CustomLink>
-        <CustomLink href={'/games'} className={style.headerLink}>Игры</CustomLink>
-        <CustomLink href={'/statistics'} className={style.headerLink}>Статистика</CustomLink>
+        <CustomLink to={'/'} className={style.headerLink}>Главная</CustomLink>
+        <CustomLink to={'/textbook'} className={style.headerLink}>Учебник</CustomLink>
+        <CustomLink to={'/games'} className={style.headerLink}>Игры</CustomLink>
+        <CustomLink to={'/statistics'} className={style.headerLink}>Статистика</CustomLink>
       </ul>
     </nav>
   </header>
 }
 
-const CustomLink = ({href, children, ...props}: { href: string, children: string, className: string }) => {
-  const path = window.location.pathname;
+const CustomLink = ({to, children, ...props}: { to: string, children: string, className: string }) => {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch( {path: resolvedPath.pathname, end: true} )
   return (
-      <li className={path === href ? style.active : ''}>
-        <a href={href} {...props}>{children}</a>
+      <li className={isActive ? style.active : ''}>
+        <Link to={to} {...props}>{children}</Link>
       </li>
   )
 }
