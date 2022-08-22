ec38ce7207941a8ea4ecc180ba8e3932784aaae2
Tatiana Kostukevich <tankostuk@gmail.com>
2022-08-22 14:06:07 +0300

    feat: add router

---
+++ src/services/token.ts
@@ -0,0 +1,44 @@
+let token: null | string = null
+let refreshToken: null | string = null
+
+const STORAGE_KEY = 'loggedRslangAppUser'
+
+interface User {
+  message: string
+  refreshToken: string
+  token: string
+  userId: string
+}
+
+const setUser = (user: User) => {
+  window.localStorage.setItem(STORAGE_KEY, JSON.stringify(user))
+  token = user.token
+  refreshToken = user.refreshToken
+}
+
+const getUser = () => {
+  const loggedUserJSON = window.localStorage.getItem(STORAGE_KEY)
+  if (loggedUserJSON) {
+    const user = JSON.parse(loggedUserJSON)
+    token = user.token
+    return user
+  }
+
+  return null
+}
+
+const clearUser = () => {
+  localStorage.clear()
+  token = null
+}
+
+const getToken = () => token
+const getRefreshToken = () => refreshToken
+
+export default {
+  setUser,
+  getUser,
+  clearUser,
+  getToken,
+  getRefreshToken
+}
--- src/services/words.ts
+++ src/services/words.ts
@@ -2,14 +2,19 @@ import axios from "axios"
 
 const baseUrl = 'http://localhost:3001'
 
+const getWords = (page: string, group: string) => {
+  const request = axios.get(`${baseUrl}/words?page=${page}&group=${group}`)
+  return request.then((response) => response.data)
+}
+
+const getWord = (id: string) => {
+  const request = axios.get(`${baseUrl}/words/${id}`)
+  return request.then((response) => response.data)
+}
+
 const getAllWords = () => {
   const request = axios.get(`${baseUrl}/words`)
   return request.then((response) => response.data)
 }
 
-const getOneWord = (id: string) => {
-  const request = axios.get(`${baseUrl}/words/${id}`)
-  return request.then((response) => response.data)
-}
-
-export default { getAllWords, getOneWord }
+export default { getAllWords, getWord, getWords }
