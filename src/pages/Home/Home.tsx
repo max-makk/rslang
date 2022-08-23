3b8d27ee85f5375251c8ca9253e274e3a9ab854e
Kate Hubar <70864294+KateHubar@users.noreply.github.com>
2022-08-23 14:02:12 +0300

    feat:  adjust app-structure and home-page-layout

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,26 +1,13 @@
+import { Form } from '../../components/Login-form/Form';
+import { Team } from '../../components/Team/Team';
+import { Description } from '../../components/Description/Description';
 import style from './Home.module.css';
 
 export const Home = () => {
-  return (
-    <div className={style.team_wrapper}>
-      <div className={style.team_string}>
-        <a href="https://github.com/max-makk" target="_blank" rel="noreferrer">
-          <div className={style.team_ava + ' ' + style.team_ava1}></div>
-        </a>
-        <article className={style.team_text}>Team Lead, Frontend developer. Координация работы комды. Разработка архитектуры приложения. Настройка сервера и работы с ним.</article>
-      </div>
-      <div className={style.team_string}>
-        <a href="https://github.com/KateHubar" target="_blank" rel="noreferrer">
-          <div className={style.team_ava + ' ' + style.team_ava2}></div>
-        </a>
-        <article className={style.team_text}>Frontend developer. Разработка главной страницы и формы авторизации.</article>
-      </div>
-      <div className={style.team_string}>
-        <a href="https://github.com/tankost" target="_blank" rel="noreferrer">
-          <div className={style.team_ava + ' ' + style.team_ava3}></div>
-        </a>
-        <article className={style.team_text}>Frontend developer. Разработка хедера и меню. Разработка словаря. </article>
-      </div>
+  return <>
+    <div className={style.home_wrapper}><Form/>
+    <Description/>
     </div>
-  );
-};
+    <Team/>
+  </>;
+}
