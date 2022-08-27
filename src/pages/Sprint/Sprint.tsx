20696e962e5aa4b80db32181a974a1a00f1fe4ab
max-makk <90equals50@gmail.com>
2022-08-27 09:27:54 +0300

    feat: style for buttons

--- src/pages/Sprint/Sprint.tsx
+++ src/pages/Sprint/Sprint.tsx
@@ -1,4 +1,12 @@
+import style from './Sprint.module.css'
 
 export const Sprint = () => {
-  return <div>Спринт</div>
+  return <div className={style.buttons}>
+    <button className={`${style.btn} ${style.lvl1}`}>1</button>
+    <button className={`${style.btn} ${style.lvl2}`}>2</button>
+    <button className={`${style.btn} ${style.lvl3}`}>3</button>
+    <button className={`${style.btn} ${style.lvl4}`}>4</button>
+    <button className={`${style.btn} ${style.lvl5}`}>5</button>
+    <button className={`${style.btn} ${style.lvl6}`}>6</button>
+  </div>
 }
