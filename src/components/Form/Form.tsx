143a6cd5ebdc905270a657d1b53d229a73af649a
Kate Hubar <70864294+KateHubar@users.noreply.github.com>
2022-08-29 00:32:30 +0300

    feat: add form action and style

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,28 +1,88 @@
-import { useAppDispatch, useAppSelector } from "../../state/hooks"
-import { SyntheticEvent, useEffect, useState } from "react"
-import userService from '../../services/user'
-import loginService from '../../services/login'
-import registerService from '../../services/register'
-import { loginUser, logoutUser } from '../../state/reducers/user'
+import { useAppDispatch, useAppSelector } from '../../state/hooks';
+import { useEffect, useState } from 'react';
+import userService from '../../services/user';
+import loginService from '../../services/login';
+import registerService from '../../services/register';
+import { loginUser, logoutUser } from '../../state/reducers/user';
+import style from './Form.module.css';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+
+const regSchema = yup
+  .object({
+    username: yup
+      .string()
+      .min(2, 'Too Short!')
+      .max(50, 'Too Long!')
+      .required('Required'),
+    password: yup
+      .string()
+      .min(8, 'Too Short! Use 8 symbols and more.')
+      .max(50, 'Too Long!')
+      .required('Required'),
+    email: yup.string().email('Invalid email').required('Required'),
+  })
+  .required();
+
+const loginSchema = yup
+  .object({
+    password: yup
+      .string()
+      .min(8, 'Too Short! Use 8 symbols and more.')
+      .max(50, 'Too Long!')
+      .required('Required'),
+    email: yup.string().email('Invalid email').required('Required'),
+  })
+  .required();
+
+type LoginFormData = {
+  password: string;
+  email: string;
+};
+
+type RegistrationFormData = LoginFormData & {
+  username: string;
+};
 
 export const Form = () => {
-  const dispatch = useAppDispatch()
-  const user = useAppSelector((state) => state.user)
+  const dispatch = useAppDispatch();
+  const user = useAppSelector((state) => state.user);
 
-  const [loginVisible, setLoginVisible] = useState(false)
-  const hideWhenVisible = { display: loginVisible ? 'none' : '' }
-  const showWhenVisible = { display: loginVisible ? '' : 'none' }
+  const [loginVisible, setLoginVisible] = useState(false);
+  const hideWhenVisible = { display: loginVisible ? 'none' : '' };
+  const showWhenVisible = { display: loginVisible ? '' : 'none' };
+
+  useEffect(() => {
+    const userFromStorage = userService.getUser()
+    if (userFromStorage) {
+      dispatch(loginUser(userFromStorage))
+    }
+  }, []);
 
   const logout = () => {
-    userService.clearUser()
-    dispatch(logoutUser(null))
-  }
+    userService.clearUser();
+    dispatch(logoutUser(null));
+  };
 
   if (user === null) {
     return (
-      <div>
-          <button onClick={() => setLoginVisible(true)}>reg</button>
-          <button onClick={() => setLoginVisible(false)}>login</button>
+      <div className={style.login_form}>
+        <div className={style.login_form_header}>
+          <button
+            onClick={() => setLoginVisible(true)}
+            className={style.login_form_header_p}
+          >
+            Регистрация
+          </button>
+          <span>|</span>
+          <button
+            onClick={() => setLoginVisible(false)}
+            className={style.login_form_header_p}
+          >
+            Вход
+          </button>
+        </div>
         <div style={hideWhenVisible}>
           <Login />
         </div>
@@ -30,123 +90,146 @@ export const Form = () => {
           <Registration displayLogin={setLoginVisible} />
         </div>
       </div>
-    )
+    );
   }
-  return <div>
-    имя того кто вошел
-    <button onClick={logout}>выйти</button>
-  </div>
-}
-
+  return (
+    <div className={style.login_form + ' ' + style.login_form_greeting}>
+      Привет, {(user as {name: string}).name}!
+      <button onClick={logout} className={style.login_form_input + ' ' + style.login_form_btn}>Выйти</button>
+    </div>
+  );
+};
 
 const Login = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const dispatch = useAppDispatch()
+  /* const [email, setEmail] = useState('');
+  const [password, setPassword] = useState(''); */
+  const dispatch = useAppDispatch();
 
-  const handleSubmit = (event: SyntheticEvent) => {
-    event.preventDefault()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormData>({
+    resolver: yupResolver(loginSchema),
+  });
+
+  const login = ({ email, password }: LoginFormData) => {
+    // event.preventDefault();
     // onLogin(username, password)
     loginService
-    .login({
-      email: email,
-      password: password
-    })
-    .then((user) => {
-      userService.setUser(user)
+      .login({
+        email: email,
+        password: password,
+      })
+      .then((user) => {
+        userService.setUser(user);
 
-      dispatch(loginUser(user))
-      // notify(`${user.name} logged in!`)
-    })
-    .catch(() => {
-      // notify('wrong username/password', 'alert')
-    })
-  }
+        dispatch(loginUser(user));
+        // notify(`${user.name} logged in!`)
+      })
+      .catch(() => {
+        // notify('wrong username/password', 'alert')
+      });
+  };
 
   return (
     <div>
-      <h2>Log in to application</h2>
+      {/*     <h2>Log in to application</h2> */}
 
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit((data) => login(data))}>
         <div>
-          email
           <input
-            value={email}
-            onChange={({ target }) => setEmail(target.value)}
+            className={style.login_form_input}
+            placeholder="E-mail"
+            {...register('email')}
           />
+          <p className={style.red}>{errors.email?.message}</p>
         </div>
         <div>
-          password
           <input
+            className={style.login_form_input}
             type="password"
-            value={password}
-            onChange={({ target }) => setPassword(target.value)}
+            placeholder="Пароль"
+            {...register('password')}
           />
+          <p className={style.red}>{errors.password?.message}</p>
         </div>
-        <button type="submit">
-          login
+        <button
+          type="submit"
+          className={style.login_form_input + ' ' + style.login_form_btn}
+        >
+          Войти
         </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
 type showLogin = {
-  displayLogin: (value: boolean) => void
-}
+  displayLogin: (value: boolean) => void;
+};
 
 const Registration = ({ displayLogin }: showLogin) => {
-  const [username, setUsername] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegistrationFormData>({
+    resolver: yupResolver(regSchema),
+  });
 
-  const handleSubmit = (event: SyntheticEvent) => {
-    event.preventDefault()
-
-    registerService.createUser({
-      name: username,
-      email: email,
-      password: password
-    })
-    .then(() => displayLogin(false))
-    .catch((err) => console.log(err))
+  const createUser = ({username, email, password}: RegistrationFormData) => {
+    registerService
+      .createUser({
+        name: username,
+        email: email,
+        password: password,
+      })
+      .then(() => displayLogin(false))
+      .catch((err) => console.log(err));
     // onLogin(username, password)
-  }
+  };
 
   return (
     <div>
-      <h2>Reg</h2>
+      {/*       <h2>Reg</h2> */}
 
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit((data) => createUser(data))}>
         <div>
-          name
           <input
-            value={username}
-            onChange={({ target }) => setUsername(target.value)}
+            className={style.login_form_input}
+            placeholder="Имя"
+            {...register('username')}
           />
+          <p className={style.red}>{errors.username?.message}</p>
         </div>
         <div>
-          email
           <input
-            value={email}
-            onChange={({ target }) => setEmail(target.value)}
+            className={style.login_form_input}
+            placeholder="E-mail"
+            {...register('email')}
           />
+          <p className={style.red}>{errors.email?.message}</p>
         </div>
         <div>
-          password
           <input
+            className={style.login_form_input}
             type="password"
-            value={password}
-            onChange={({ target }) => setPassword(target.value)}
+            placeholder="Пароль"
+            {...register('password')}
           />
+          <p className={style.red}>{errors.password?.message}</p>
         </div>
-        <button type="submit">
-          registr
+        <button
+          type="submit"
+          className={style.login_form_input + ' ' + style.login_form_btn}
+        >
+          Зарегистрироваться
         </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
 
 // import { useForm } from 'react-hook-form';
@@ -164,7 +247,7 @@ const Registration = ({ displayLogin }: showLogin) => {
 //     .required('Required'),
 //     password: yup
 //       .string()
-//       .min(6, 'Too Short! Use 6 symbols and more.')
+//       .min(8, 'Too Short! Use 8 symbols and more.')
 //       .max(50, 'Too Long!')
 //       .required('Required'),
 //     email: yup.string().email('Invalid email').required('Required'),
@@ -177,7 +260,7 @@ const Registration = ({ displayLogin }: showLogin) => {
 //     //.required('Required')
 //     password: yup
 //       .string()
-//       .min(6, 'Too Short! Use 6 symbols and more.')
+//       .min(8, 'Too Short! Use 8 symbols and more.')
 //       .max(50, 'Too Long!')
 //       .required('Required'),
 //     email: yup.string().email('Invalid email').required('Required'),
