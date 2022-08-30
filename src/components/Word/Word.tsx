2924d2fbd4e55440dfc73d92afee5e645a663188
Tatiana Kostukevich <tankostuk@gmail.com>
2022-08-31 01:18:02 +0300

    feat: add colors in words cards

--- src/components/Word/Word.tsx
+++ src/components/Word/Word.tsx
@@ -1,8 +1,11 @@
 import style from './Word.module.css';
 import { IWord } from "../../types/types";
 import MusicNoteRoundedIcon from '@mui/icons-material/MusicNoteRounded';
+import { useState } from 'react';
 
 export const Word = (word: IWord) => {
+  const [color] = useState(`var(--level${word.group + 1})`);
+  const [backgroundColor] = useState(`var(--level${word.group + 1})`);
   const baseUrl = 'http://localhost:3001'
 
   const playAudio = () => {
@@ -27,17 +30,21 @@ export const Word = (word: IWord) => {
         </div>
         <div className={style.info}>
           <div className={style.headings}>
-            <span className={style.heading}>{word.word}</span>
+            <span style={{backgroundColor}} className={style.heading}>{word.word}</span>
             <span className={style.heading}>{word.transcription}</span>
             <button className={style.audio_button} onClick={() => playAudio()}>
-              <MusicNoteRoundedIcon className={style.audio}/>
+              <MusicNoteRoundedIcon style={{color}} className={style.audio}/>
             </button>
           </div>
-          <div className={style.translate_word}>{word.wordTranslate}</div>
-          <div className={style.example} dangerouslySetInnerHTML={{__html: word.textMeaning}}/>
-          <div className={style.translate_example}>{word.textMeaningTranslate}</div>
-          <div className={style.example} dangerouslySetInnerHTML={{__html: word.textExample}}/>
-          <div className={style.translate_example}>{word.textExampleTranslate}</div>
+          <div style={{color}} className={style.translate_word}>{word.wordTranslate}</div>
+          <div className={style.translate_sentences}>
+            <div className={style.example} dangerouslySetInnerHTML={{__html: word.textMeaning}}/>
+            <div className={style.translate_example}>{word.textMeaningTranslate}</div>
+          </div>
+          <div className={style.translate_sentences}>
+            <div className={style.example} dangerouslySetInnerHTML={{__html: word.textExample}}/>
+            <div className={style.translate_example}>{word.textExampleTranslate}</div>
+          </div>
         </div>
       </div>
   )
