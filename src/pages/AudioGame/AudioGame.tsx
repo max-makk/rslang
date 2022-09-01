46c91c8dfe65e31f86df77ec8b3744df14dd744a
Kate Hubar <70864294+KateHubar@users.noreply.github.com>
2022-09-02 01:51:42 +0300

    feat: AudioGame layout

--- src/pages/AudioGame/AudioGame.tsx
+++ src/pages/AudioGame/AudioGame.tsx
@@ -1,19 +1,33 @@
+import { useAppDispatch } from '../../state/hooks';
+import { setGroup } from '../../state/reducers/textbook';
 import style from './AudioGame.module.css';
+import MusicNoteRoundedIcon from '@mui/icons-material/MusicNoteRounded';
+import { IWord } from "../../types/types";
 
 export const AudioGame = () => {
+  const dispatch = useAppDispatch();
+
+  const baseUrl = 'http://localhost:3001'
+ 
+  /* const playAudio = () => {
+    const audioWord = new Audio(`${baseUrl}/${word.audio}`)
+  } */
   return (
     <div className={style.audiogame_wrapper}>
       Выберите уровень:
       <div className={style.audiogame_level_wrapper}>
-        <button className={style.audiogame_level}>1</button>
-        <button className={style.audiogame_level}>2</button>
-        <button className={style.audiogame_level}>3</button>
-        <button className={style.audiogame_level}>4</button>
-        <button className={style.audiogame_level}>5</button>
-        <button className={style.audiogame_level}>6</button>
+        <button className={style.audiogame_level} onClick={() => dispatch(setGroup('0'))}>1</button>
+        <button className={style.audiogame_level} onClick={() => dispatch(setGroup('1'))}>2</button>
+        <button className={style.audiogame_level} onClick={() => dispatch(setGroup('2'))}>3</button>
+        <button className={style.audiogame_level} onClick={() => dispatch(setGroup('3'))}>4</button>
+        <button className={style.audiogame_level} onClick={() => dispatch(setGroup('4'))}>5</button>
+        <button className={style.audiogame_level} onClick={() => dispatch(setGroup('5'))}>6</button>
       </div>
       <div className={style.audiogame_gamewindow}>
-       <div className={style.audiogame_gamewindow_main_wrapper}> <div className={style.audiogame_gamewindow_main}>знак ноты и замена на карточку</div> </div>
+       <div className={style.audiogame_gamewindow_main_wrapper}> <div className={style.audiogame_gamewindow_main}> 
+       <button className={style.audiogame_gamewindow_sound} >
+        <MusicNoteRoundedIcon className={style.audio}/>
+        </button> знак ноты и замена на карточку</div> </div>
         <div className={style.audiogame_gamewindow_words_string}>
           <div className={style.audiogame_gamewindow_word}>word_1</div>
           <div className={style.audiogame_gamewindow_word}>word_2</div>
